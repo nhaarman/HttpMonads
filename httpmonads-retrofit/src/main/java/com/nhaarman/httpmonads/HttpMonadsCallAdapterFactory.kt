e1f819efd5fd894bef9c8834433f082801321d5a
Niek Haarman <haarman.niek@gmail.com>
2020-03-03 21:27:23 +0100

    wup

--- httpmonads-retrofit/src/main/java/com/nhaarman/httpmonads/HttpMonadsCallAdapterFactory.kt
+++ httpmonads-retrofit/src/main/java/com/nhaarman/httpmonads/HttpMonadsCallAdapterFactory.kt
@@ -4,8 +4,9 @@ import com.nhaarman.httpmonads.arrow.EitherCallAdapter
 import com.nhaarman.httpmonads.funktionale.DisjunctionCallAdapter
 import com.nhaarman.httpmonads.internal.getParameterUpperBound
 import com.nhaarman.httpmonads.internal.rawTypeFor
-import retrofit2.CallAdapter
-import retrofit2.Retrofit
+import okhttp3.Request
+import retrofit2.*
+import java.io.IOException
 import java.lang.reflect.ParameterizedType
 import java.lang.reflect.Type
 
@@ -31,6 +32,7 @@ class HttpMonadsCallAdapterFactory private constructor(
         fun create(): HttpMonadsCallAdapterFactory {
             return HttpMonadsCallAdapterFactory(
                 mapOf(
+                    //Call::class.java to ResultAdapter.Companion::create,
                     HttpTry::class.java to HttpTryCallAdapter.Companion::create,
                     typeFor("arrow.core.Either") to EitherCallAdapter.Companion::create,
                     typeFor("org.funktionale.either.Disjunction") to DisjunctionCallAdapter.Companion::create
@@ -46,4 +48,75 @@ class HttpMonadsCallAdapterFactory private constructor(
             }
         }
     }
+}
+
+abstract class CallDelegate<TIn, TOut>(
+    protected val proxy: Call<TIn>
+) : Call<TOut> {
+
+    override fun execute(): Response<TOut> = throw NotImplementedError()
+    override final fun enqueue(callback: Callback<TOut>) = enqueueImpl(callback)
+    override final fun clone(): Call<TOut> = cloneImpl()
+
+    override fun cancel() = proxy.cancel()
+    override fun request(): Request = proxy.request()
+    override fun isExecuted() = proxy.isExecuted
+    override fun isCanceled() = proxy.isCanceled
+
+    abstract fun enqueueImpl(callback: Callback<TOut>)
+    abstract fun cloneImpl(): Call<TOut>
+}
+
+class ResultAdapter(
+    private val type: Type
+) : CallAdapter<Type, Call<HttpTry<Type>>> {
+
+    override fun responseType() = type
+    override fun adapt(call: Call<Type>): Call<HttpTry<Type>> = ResultCall(type, call)
+
+    companion object {
+
+        fun create(responseType: Type) = ResultAdapter(responseType)
+    }
+
+    class ResultCall<T>(
+        private val responseType: Type,
+        proxy: Call<T>
+    ) : CallDelegate<T, HttpTry<T>>(proxy) {
+
+        override fun enqueueImpl(callback: Callback<HttpTry<T>>) = proxy.enqueue(object : Callback<T> {
+
+            override fun onResponse(call: Call<T>, response: Response<T>) {
+                val result = when {
+                    responseType == Unit::class.java && response.isSuccessful -> successfulUnitResult()
+                    response.isSuccessfulWithBody() -> HttpTry.Success(response.body()!!)
+                    else -> HttpTry.Failure(response.toHttpError())
+                }
+
+                callback.onResponse(this@ResultCall, Response.success(result))
+            }
+
+            @Suppress("UNCHECKED_CAST")
+            private fun successfulUnitResult(): HttpTry<T> {
+                return HttpTry.Success(Unit) as HttpTry<T>
+            }
+
+            private fun <R> Response<R>.isSuccessfulWithBody(): Boolean {
+                return code() in 200..299 && code() != 204 && code() != 205
+            }
+
+            override fun onFailure(call: Call<T>, t: Throwable) {
+                val result = if (t is IOException) {
+                    HttpTry.Failure(HttpError.NetworkError(t))
+                } else {
+                    throw t
+                }
+
+                callback.onResponse(this@ResultCall, Response.success(result))
+            }
+        })
+
+        override fun cloneImpl() = ResultCall(responseType, proxy.clone())
+    }
+
 }
---
+++ httpmonads-retrofit/src/test/java/com/nhaarman/httpmonads/RetrofitIntegrationTest.kt
@@ -0,0 +1,78 @@
+package com.nhaarman.httpmonads
+
+import com.nhaarman.expect.expect
+import okhttp3.Interceptor
+import okhttp3.Interceptor.Chain
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import okhttp3.Protocol.HTTP_1_0
+import okhttp3.ResponseBody
+import org.junit.Test
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+class RetrofitIntegrationTest {
+
+    private val service = Retrofit.Builder()
+        .baseUrl("https://localhost")
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .addCallAdapterFactory(HttpMonadsCallAdapterFactory.create())
+        .client(OkHttpClient.Builder()
+            .addInterceptor(object : Interceptor {
+                override fun intercept(chain: Chain): okhttp3.Response {
+                    return okhttp3.Response.Builder()
+                        .request(chain.request())
+                        .protocol(HTTP_1_0)
+                        .code(200)
+                        .message("OK")
+                        .body(ResponseBody.create(MediaType.parse("text/plain"),"success!"))
+                        .build()
+                }
+            })
+            .build())
+        .build()
+        .create(Service::class.java)
+
+
+    @Test
+    fun `Call of String can be executed`( ){
+        /* When */
+        val result = service.callResult().execute().body()
+
+        /* Then */
+        expect(result).toBe("success!")
+    }
+
+    @Test
+    fun `Call of HttpTry can be executed`( ){
+        /* When */
+        val result = service.callHttpTryResult().execute().body()
+
+        /* Then */
+        expect(result).toBe(HttpTry.success("success!"))
+    }
+
+    @Test
+    fun `Response of HttpTry can be executed`( ){
+        /* When */
+        val result = service.responseHttpTryResult().body()
+
+        /* Then */
+        expect(result).toBe(HttpTry.success("success!"))
+    }
+
+    interface Service {
+
+        @GET("/")
+        fun callResult(): Call<String>
+
+        @GET("/")
+        fun callHttpTryResult() : Call<HttpTry<String>>
+
+        @GET("/")
+        fun responseHttpTryResult() : Response<HttpTry<String>>
+    }
+}
